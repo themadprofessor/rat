69af16383da03dc1068b6ac509f171dff7dfd6e6
Stuart Reilly <stu@reilly-family.co.uk>
2017-08-07 22:17:22 +0100

    Abstract input Add clap Extend test.txt

---
+++ src/input.rs
@@ -0,0 +1,33 @@
+use std::io::{Read, BufRead, BufReader, StdinLock};
+use std::io;
+use std::fs::File;
+
+pub enum Input<'a> {
+    File(BufReader<File>),
+    Stdin(StdinLock<'a>)
+}
+
+impl <'a> Read for Input<'a> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        match *self {
+            Input::File(ref mut f) => f.read(buf),
+            Input::Stdin(ref mut i) => i.read(buf)
+        }
+    }
+}
+
+impl <'a> BufRead for Input<'a> {
+    fn fill_buf(&mut self) -> io::Result<&[u8]> {
+        match *self {
+            Input::File(ref mut f) => f.fill_buf(),
+            Input::Stdin(ref mut i) => i.fill_buf()
+        }
+    }
+
+    fn consume(&mut self, amt: usize) {
+        match *self {
+            Input::File(ref mut f) => f.consume(amt),
+            Input::Stdin(ref mut i) => i.consume(amt)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,57 @@
+extern crate clap;
+
 use std::io;
 use std::io::BufReader;
 use std::fs::File;
-use std::env;
+
+use clap::{Arg, App};
+
+use input::Input;
+
+mod input;
 
 fn main() {
-    for arg in env::args().skip(1) {
-        let mut read = match File::open(arg) {
-            Ok(f) => BufReader::new(f),
-            Err(e) => exit(-1, &e.to_string())
-        };
-        let stdout = io::stdout();
-        let mut lock = stdout.lock();
-        match io::copy(&mut read, &mut lock) {
-            Ok(_) => (),
-            Err(e) => exit(-2, &e.to_string())
-        };
+    let matches = App::new("Rat")
+        .version("0.1.0")
+        .author("Stuart Reilly")
+        .about("Rust implementation of cat")
+        .arg(Arg::with_name("files")
+            .takes_value(true)
+            .multiple(true))
+        .arg(Arg::with_name("tabs")
+            .short("T")
+            .long("show-tabs"))
+        .get_matches();
+
+
+    let stdout = io::stdout();
+    let stdin = io::stdin();
+    let mut lock = stdout.lock();
+
+    if !matches.is_present("files") {
+        let stdin = io::stdin();
+        let mut input = Input::Stdin(stdin.lock());
+        write(&mut input, &mut lock);
+        return;
+    }
+
+    for arg in matches.values_of("files").unwrap() {
+        let mut read = BufReader::new(if arg == "-" {
+            Input::Stdin(stdin.lock())
+        } else {
+            Input::File(match File::open(arg) {
+                Ok(f) => BufReader::new(f),
+                Err(e) => exit(-1, &e.to_string())
+            })
+        });
+        write(&mut read, &mut lock)
+    }
+}
+
+fn write<T>(input: &mut T, out: &mut io::StdoutLock) where T: io::Read {
+    match io::copy(input, out) {
+        Ok(_) => (),
+        Err(e) => exit(-2, &e.to_string())
     }
 }
 
